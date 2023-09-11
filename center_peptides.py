af81c2cca27bdfe12db1dcb4b16c8307cb0cc6a8
David Mast <david.mast@pnnl.gov>
2023-09-11 11:47:58 -0700

    added ability to randomize more complex regex patterns

--- center_peptides.py
+++ center_peptides.py
@@ -7,9 +7,42 @@ Created on Sat Sep  2 09:16:34 2023
 
 import re
 from dataclasses import dataclass
+from collections import OrderedDict
+import string_permutations
+import random
 
+def generate_random_peptide_list(num_peptides, min_length=6, max_length=15):
+    
+    '''
+    
+    Generate a list of random peptide sequences with specified characteristics.
 
+    Args:
+        num_peptides (int): The number of random peptides to generate.
+        min_length (int, optional): The minimum length of a peptide sequence (default is 6).
+        max_length (int, optional): The maximum length of a peptide sequence (default is 15).
 
+    Returns:
+        list: A list of randomly generated peptide sequences.
+
+    A peptide sequence is composed of amino acids selected from a predefined set of
+    amino acids. This function generates 'num_peptides' random peptide sequences,
+    each with a length between 'min_length' and 'max_length' (inclusive).
+
+    Example:
+    >>> generate_random_peptide_list(10)
+    ['PKMENL', 'YFIVLNT', 'ATWHTESM', 'RDYQLMST', 'VLEKFGHYR', 'MVESWI', 'VKSFIYD',
+    'QMVFWIGY', 'RVTLMYCQI', 'SNMWVI']
+    
+    '''
+    # Define the amino acids you want to include
+    amino_acids = "ACDEFGHIKLMNPQRSTVWY"
+    peptide_list = []
+    for _ in range(num_peptides):
+        peptide_length = random.randint(min_length, max_length)
+        peptide = ''.join(random.choice(amino_acids) for _ in range(peptide_length))
+        peptide_list.append(peptide)
+    return peptide_list
 
 
 @dataclass
@@ -22,6 +55,7 @@ class CenteredPeptideParams:
     '''
     regex_list: list
     min_seq_length: int
+    
     
 
 
@@ -46,7 +80,7 @@ def split_protein_sequence(protein_sequence):
 
     return subsequences
 
-def get_centered_peptides(expression: str, peptide_list: list, all_possible=False, return_original_seqs=False):
+def get_centered_peptides(expression: str, peptide_list: list, all_possible=False, return_counts=False, return_original_seqs=False):
     '''
     Generates aligned peptides based on a regex pattern
     Input: 
@@ -64,9 +98,14 @@ def get_centered_peptides(expression: str, peptide_list: list, all_possible=Fals
 
     pat = re.compile(expression)
     l_new = []
+    
     l_new_seq = []
     l_pat = []
-
+    
+    
+    if return_counts:
+        all_possible = True
+    
     for peptide in peptide_list:
 
         m = pat.findall(peptide)
@@ -86,9 +125,42 @@ def get_centered_peptides(expression: str, peptide_list: list, all_possible=Fals
         else:
             pass
     
-    
+    if return_counts:
+        # Create an ordered dictionary to store element counts
+        element_counts = OrderedDict()
+        
+        # Use a list comprehension to count occurrences and populate the ordered dictionary
+        [element_counts.update({element: element_counts.get(element, 0) + 1}) for element in l_new]
+        
+        # Convert the ordered dictionary to a list of tuples
+        #ordered_counts_list = list(element_counts.items())
+        
+        sequences_list = list(element_counts.keys())
+        counts_list = [_ for i, _ in element_counts.items()]
+        return sequences_list, counts_list
+        
+
     if return_original_seqs:
         return l_new, l_new_seq
     
     return l_new
+
+
+def check_regex(regex:str):
+    
+    pass
+
+# class CenteredPeptides:
+    
+#     def __init__(self, sequence_list:list, regex:str):
+        
+#         self.sequence_list = sequence_list
+#         self.regex = regex
+#         self.centered_peptides = []
+        
+#     def get_centered_peptide_combos(self):
+#         self.ceterered_peptides = get_centered_peptides(self.regex, self.sequence_list)
+        
+    
+
 
--- string_permutations.py
+++ string_permutations.py
@@ -24,6 +24,9 @@ import math
 from collections import Counter
 from itertools import permutations
 import matplotlib.pyplot as plt
+
+
+
 def num_perms_wo_repeats(regular_expression: str, other: bool = False) -> int:
     """
     Computes the number of unique permutations of a string with repeated characters.
@@ -96,12 +99,112 @@ def get_string_permutations_itertools(s:str):
     return perms
 
 
+
+
+def replace_regex_chars(input_string):
+    '''
+    replaces bracketed amino acids in a regex with a number, for shuffling
+    Parameters:
+        input_string : str
+            the input regex string.
+    
+    returns:
+        output_string : str
+            The string with replaced characters
+        substitution_mapping : dict
+            the dictionary for reversing the substitution
+    example:
+        
+
+        In: replace_regex_chars("....[AB]..[C]...")
+        
+        Out: ("....1..2...", {'0': '[AC]', '1': '[B]'}
+    '''
+    # remove start of line and end of line characters
+    if input_string[len(input_string)-1] == '$':
+        input_string = input_string.rstrip('$')
+    if input_string[0] == '^':
+        input_string = input_string.lstrip('^')
+    
+    
+    replacement_regex = r'\[\w*\]'
+    replacement = re.compile(replacement_regex)
+    chars_to_replace = list(replacement.findall(input_string))
+    
+    # Define a list of unique replacement values
+    replacement_values = list(range(len(chars_to_replace)))  # Add more values if needed
+    replacement_values = [str(i) for i in replacement_values]
+    substitution_mapping = dict(zip(replacement_values, chars_to_replace))
+    
+    # Use re.sub() with the callback function to perform the substitution
+    output_string = re.sub(r'\[\w*\]',lambda x: replacement_values.pop(0) , input_string)
+    return output_string, substitution_mapping
+
+
+def reverse_replace_regex_chars(output_string, substitution_mapping):
+    '''
+    reverses the replace_regex_chars() function. Returns the original regex string
+    '''
+    # Use re.sub() with the callback function to perform the reverse substitution
+    reversed_string = re.sub(r'\d',lambda x: substitution_mapping.get(x.group()), output_string)
+    return reversed_string
+
+def check_regex_input(input_string):
+    invalid_chars = ',!@#%&*))/ -+'
+    checks = dict(
+        has_brackets = False,
+        has_start = False,
+        has_end = False,
+    )
+    
+    bracket_regex = r'\[\w*\]'
+    m = re.compile(bracket_regex)
+    
+    if m.search(input_string):
+        checks.update(dict(has_brackets=True))
+        bracketed = m.findall(input_string)
+        print('number of substitutions to be made = ', len(m.findall(input_string)))
+        
+    if input_string[0] == '^':
+        checks.update(dict(has_start=True))
+    
+    if input_string[len(input_string)-1] == '$':
+        checks.update(dict(has_end=True))
+    
+    return checks
+    
+
+
+def convert_regex_input(input_string):
+    
+    
+    #check for bracketed words
+    bracket_regex = r'\[\w*\]'
+    m = re.compile(bracket_regex)
+    if m.search(input_string):
+        
+        print('Input string contains match')
+        try:
+            output_string, output_mapping = replace_regex_chars(input_string)
+        except TypeError as err:
+            print(err)
+            raise TypeError('replace_regex_chars() function encountered an error. Inspect the input_string')
+        return output_string, output_mapping
+    else:
+        return input_string, False
+    
+
+
 def randomize_string(input_string):
+    
     '''
     Parameters: 
         s : (str)
             - a string to shuffle
     '''    
+    
+    
+    
     # Convert the string to a list of characters for shuffling
     char_list = list(input_string)
 
@@ -112,6 +215,8 @@ def randomize_string(input_string):
     randomized_string = ''.join(char_list)
     
     return randomized_string
+
+
 
 def randomize_string_by_number(input_string):
     char_list = list(input_string)
@@ -127,7 +232,6 @@ def randomize_string_by_number(input_string):
     return randomized_string
 
 
-
 def randomize_string_no_repeats(input_string):
     '''slowest option'''
     char_list = list(input_string)
@@ -139,11 +243,9 @@ def randomize_string_no_repeats(input_string):
         yield randomized_string
 
 
-
 def get_randomized_strings(s, by=None, create_plot=False):
-    '''
     
-
+    '''
     Parameters
     ----------
     s : string
@@ -171,6 +273,9 @@ def get_randomized_strings(s, by=None, create_plot=False):
         In: print(x)
         Out: ['BAC', 'CBA', 'ACB', 'BCA', 'CAB', 'ABC'] 
     '''
+    checks = check_regex_input(s)
+    s, output_mapping = convert_regex_input(s)
+    print(s)
     total_perms = num_perms_wo_repeats(s)
     if total_perms > 400_000:
         print('WAY TOO MANY PERMUTATIONS! try a different string!')
@@ -218,6 +323,14 @@ def get_randomized_strings(s, by=None, create_plot=False):
         
     print('calculated_number of combos: ', len(hits))
     print('hits-to-iterations ratio : ', f'{len(hits)/i:0.2f}')
+    if output_mapping:
+        hits = [reverse_replace_regex_chars(_, output_mapping) for _ in hits]
+        
+    if checks.get('has_start'):
+        hits = ['^'+hit for hit in hits]
+    if checks.get('has_end'):
+        hits = [hit+'$' for hit in hits]
+         
     return hits
 
 
